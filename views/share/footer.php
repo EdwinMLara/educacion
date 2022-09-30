9cd73dbd388312088379f80520968c1b483766f4
EdwinMLara <emlara35@gmail.com>
2022-09-30 15:09:25 -0500

    compartidos para plantillas

---
+++ views/share/footer.php
@@ -0,0 +1,61 @@
+<!-- Footer -->
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Municipio de uriangato GTO</span>
+        </div>
+    </div>
+</footer>
+<!-- End of Footer -->
+
+</div>
+<!-- End of Content Wrapper -->
+
+</div>
+<!-- End of Page Wrapper -->
+
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
+
+<!-- Logout Modal-->
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+            <div class="modal-footer">
+                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                <a class="btn btn-primary" href="login.html">Logout</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Bootstrap core JavaScript-->
+<script src="/educacion/vendor/jquery/jquery.min.js"></script>
+<script src="/educacion/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="/educacion/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="/educacion/js/sb-admin-2.min.js"></script>
+
+<!-- Page level plugins -->
+<script src="/educacion/vendor/chart.js/Chart.min.js"></script>
+<script src="/educacion/js/jquery.validate.min.js"></script>
+<script src="/educacion/js/usuarios.js"></script>
+<!-- Page level custom scripts 
+<script src="/educacion/js/demo/chart-area-demo.js"></script>
+<script src="/educacion/js/demo/chart-pie-demo.js"></script>-->
+
+</body>
+
+</html>
