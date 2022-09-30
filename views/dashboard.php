82a704c2f4cdb6a68009236b3b9101015bb8890a
EdwinMLara <emlara35@gmail.com>
2022-09-30 15:15:44 -0500

    sin nada aun

---
+++ views/dashboard.php
@@ -0,0 +1,44 @@
+<?php
+
+//El login es parcial despues de va agregar su funcion
+$login = true;
+
+
+// Si no esta loggeado mostrar login
+if(!$login){
+    require_once("./views/login.php");
+    return;
+}
+
+
+// Si esta loggeado mostrar panel de administracion
+if ($login) {
+    // Cargando Header
+    require_once("./share/header.php");
+
+    // Cargando navbar
+    require_once("./share/nav.php");
+?>
+
+<!-- Begin Page Dashboard -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+    </div>
+
+    <!-- Content Row -->
+    <div class="row">
+
+</div>
+<!-- /.container-fluid -->
+
+<?php
+    // Cargando Fooder
+    require_once("./share/footer.php");
+    return;
+}
+?>
---
+++ views/share/paginador.php
@@ -0,0 +1,20 @@
+<div class="col-sm-12 col-md-5">
+    <div id="mostrarNumero" class="dataTables_info" id="dataTable_info">
+        Mostrando del 1 al N
+    </div>
+</div>
+<div class="col-sm-12 col-md-7">
+    <div id="dataTables_paginate paging_simple_number" class="dataTables_paginate">
+        <ul class="pagination">
+            <li class="page-item disabled">
+                <a class="page-link" href="#" tabindex="-1">Previous</a>
+            </li>
+            <li class="page-item active"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item">
+                <a class="page-link" href="#">Next</a>
+            </li>
+        </ul>
+    </div>
+</div>
