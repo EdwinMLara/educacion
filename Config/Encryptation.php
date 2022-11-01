d0f29511c994260e74b6c454f8116bc5d37614cb
EdwinMLara <emlara35@gmail.com>
2022-11-01 15:15:33 -0600

    se cambio sistema de encryptacion, otro no funcionada

--- Config/Encryptation.php
+++ Config/Encryptation.php
@@ -1,23 +1,25 @@
 <?php
-    class Encrytation{
-        public static $method = 'AES-256-CBC';
-        public function __construct(){
-        }
+class Encrytation
+{
+    public static $method = 'AES-128-CTR';
 
-        public static function encrypt(string $data, string $key): string{
-            $ivSize = openssl_cipher_iv_length(static::$method);
-            $iv = openssl_random_pseudo_bytes($ivSize);
-            $encrypted = openssl_encrypt($data, static::$method, $key, OPENSSL_RAW_DATA, $iv);
-            $encrypted = strtoupper(implode(null, unpack('H*', $encrypted)));
-            return $encrypted;
-        }
-        
-        public static function decrypt(string $data, string $key): string{
-            $data = pack('H*', $data);
-            $ivSize = openssl_cipher_iv_length(static::$method);  
-                $iv = $iv = openssl_random_pseudo_bytes($ivSize);
-            $decrypted = openssl_decrypt($data, static::$method, $key, OPENSSL_RAW_DATA, $iv); 
-            return trim($decrypted);
-        }
+    public function __construct()
+    {
     }
-?>
+
+    public static function encrypt(string $data, string $key): string
+    {
+        $ivSize = openssl_cipher_iv_length(static::$method);
+        $iv = openssl_random_pseudo_bytes($ivSize);
+        $encrypted = openssl_encrypt($data, static::$method, $key, 0, '1234567891011121');
+        return $encrypted;
+    }
+
+    public static function decrypt(string $data, string $key): string
+    {
+        $ivSize = openssl_cipher_iv_length(static::$method);
+        $iv = $iv = openssl_random_pseudo_bytes($ivSize);
+        $decrypted = openssl_decrypt($data, static::$method, $key, 0, '1234567891011121');    
+        return $decrypted;
+    }
+}
