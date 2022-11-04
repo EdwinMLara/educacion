ff7564737caa30576374c79becd8120dd4e79b06
EdwinMLara <emlara35@gmail.com>
2022-11-04 14:00:50 -0600

    metodo para paginar solicitudes

--- routers/solicitudesRouter.php
+++ routers/solicitudesRouter.php
@@ -2,6 +2,11 @@
 require_once("../Config/RestApi.php");
 require_once("../Config/Service.php");
 require_once("../Modelos/Solicitudes.php");
+require_once("../Modelos/Alumnos.php");
+require_once("../Modelos/EscuelaInstitucion.php");
+require_once("../Modelos/DatosPadre.php");
+require_once("../Modelos/IngresosFamiliares.php");
+require_once("../Modelos/Servicios.php");
 
 class SolicitudesRouter extends RestApi {
     public function __construct(){
@@ -24,6 +29,9 @@ class SolicitudesRouter extends RestApi {
             $this->throwError(CREATED_ERROR, "An error has been ocurred to register.");
         }
     }
+
+    /** Los update se pueden eliminar utlizando el modelo sin declarar attributos para 
+    ** crearlos dinamicanmente se podria trabajar con update Solicitud*/
 
     public function updateSolicitudIdEscuela(){
         $idSolicitud = $this->validateParameter('idSolicitud',$this->param["idSolicitud"],INTEGER);
@@ -80,6 +88,9 @@ class SolicitudesRouter extends RestApi {
         }
     }
 
+    /**Hasta qui codigo basura  */
+
+
     public function updateSolicitud(){
         $idSolicitud = $this->validateParameter('idSolicitud',$this->param["idSolicitud"],INTEGER);
         $nivelEstudios = $this->validateParameter('nivelEstudios',$this->param["nivelEstudios"],STRING);
@@ -90,6 +101,52 @@ class SolicitudesRouter extends RestApi {
             $this->returnResponse(SUCESS_RESPONSE, "Se ha actualizado exitosamente la solicitud.");
         } else {
             $this->throwError(UPDATED_ERROR, "An error has been ocurred");
+        }
+    }
+
+    public function getSolicitudesPaginate(){
+        $page = $this->validateParameter('page', $this->param["page"], INTEGER);
+        $perPage = $this->validateParameter('perPage', $this->param['perPage'], INTEGER);
+
+        if ($solicitudes = $this->service->getAll()) {
+            $numSolicitudes = count($solicitudes);
+            $inicio = ($page - 1)*$perPage;
+            $fin = $perPage;
+            $paginatesSolicitudes = array_slice($solicitudes, $inicio, $fin);
+
+            $response = new stdClass();
+            $response->total = $numSolicitudes;
+            $response->solicitudes = $paginatesSolicitudes;
+
+            $max = sizeof($paginatesSolicitudes);
+            for($i=0;$i<$max;$i++){
+                $query = "SELECT * FROM `alumnos` WHERE idAlumno = ".$paginatesSolicitudes[$i]->idAlumno;
+                $alumno = $this->service->getByQueryTableModel($query,"Alumnos");
+                $paginatesSolicitudes[$i]->idAlumno = $alumno;
+
+                $query = "SELECT * FROM `escuelainstitucion` WHERE idEscuela = ".$paginatesSolicitudes[$i]->idEscuela;
+                $escuela = $this->service->getByQueryTableModel($query,"EscuelaInstitucion");
+                $paginatesSolicitudes[$i]->idEscuela = $escuela;
+
+                $query = "SELECT * FROM `datospadre` WHERE idPadre = ".$paginatesSolicitudes[$i]->idPadre;
+                $datosPadre = $this->service->getByQueryTableModel($query,"DatosPadre");
+                $paginatesSolicitudes[$i]->idPadre = $datosPadre;
+
+                $query = "SELECT * FROM `ingresosfamiliares` WHERE idIngresosFamiliares = ".$paginatesSolicitudes[$i]->idIngresosFamiliares;
+                $ingresosFamiliares = $this->service->getByQueryTableModel($query,"IngresosFamiliares");
+                $paginatesSolicitudes[$i]->idIngresosFamiliares = $ingresosFamiliares;
+
+                $query = "SELECT * FROM `servicios` WHERE idservicios = ".$paginatesSolicitudes[$i]->idServicios;
+                $servicios = $this->service->getByQueryTableModel($query,"Servicios");
+                $paginatesSolicitudes[$i]->idServicios = $servicios;
+
+                $query = "SELECT * FROM `requisitosAdicionales` WHERE idRequisitosAdicionales = ".$paginatesSolicitudes[$i]->idRequisitosAdicionales;
+                $requisitosAdicionales = $this->service->getByQueryTableModel($query,"Servicios");
+                $paginatesSolicitudes[$i]->idRequisitosAdicionales = $requisitosAdicionales;
+            }
+            $this->returnResponse(SUCESS_RESPONSE, $response);
+        } else {
+            $this->throwError('GET_ERROR', "An error has been ocurren to paginate de users");
         }
     }
 }
