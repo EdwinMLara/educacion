d3e3c208f2787871a6471d0e0c5faf67340a6c04
EdwinMLara <emlara35@gmail.com>
2023-02-17 10:38:53 -0600

    modales alert y spinner

--- views/share/modalConfirm.php
+++ views/share/modalConfirm.php
@@ -1,18 +1,10 @@
 <div class="modal mt-10" id="modalConfirm"  tabindex="-10" role="dialog">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
-      <!--<div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>-->
-      <div id="modalConfirmBody" class="modal-body">
-        
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary btn-sm" data-dismiss="modal" id="confirm-acepted-button">Aceptar</button>
-        <button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal" id="confirm-rejected-button" >Rechazar</button>
+      <div id="modalConfirmBody"> </div>
+      <div class="d-flex justify-content-end">
+        <button type="button" class="btn btn-primary btn-sm m-2" data-dismiss="modal" id="confirm-acepted-button">Aceptar</button>
+        <button type="button" class="btn btn-secondary btn-sm m-2" data-dismiss="modal" id="confirm-rejected-button" >Rechazar</button>
       </div>
     </div>
   </div>
