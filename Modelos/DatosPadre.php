0e6d96d527705803a1b1cd74ed88d8789c762040
EdwinMLara <emlara35@gmail.com>
2023-02-17 10:26:35 -0600

    cuando hay error de api doesnt exist es muy probable que sea el modelo

--- Modelos/DatosPadre.php
+++ Modelos/DatosPadre.php
@@ -8,18 +8,18 @@
             $numArgs = count($listArgs);
 
             switch($numArgs){
+                case 14:
+                    $this->__construct14($listArgs[0],$listArgs[1],$listArgs[2],$listArgs[3],$listArgs[4],$listArgs[5],$listArgs[6],$listArgs[7],$listArgs[8],$listArgs[9],$listArgs[10],$listArgs[11],$listArgs[12],$listArgs[13]);
+                    break;
                 case 15:
                     $this->__construct15($listArgs[0],$listArgs[1],$listArgs[2],$listArgs[3],$listArgs[4],$listArgs[5],$listArgs[6],$listArgs[7],$listArgs[8],$listArgs[9],$listArgs[10],$listArgs[11],$listArgs[12],$listArgs[13],$listArgs[14]);
-                    break;
-                case 16:
-                    $this->__construct16($listArgs[0],$listArgs[1],$listArgs[2],$listArgs[3],$listArgs[4],$listArgs[5],$listArgs[6],$listArgs[7],$listArgs[8],$listArgs[9],$listArgs[10],$listArgs[11],$listArgs[12],$listArgs[13],$listArgs[14],$listArgs[15]);
                     break;
                 default:
                     throw new Exception('Error al crear el constructor Padre');
             }
         }
 
-        public function __construct15($curp,$nombre,$telefono,$fechaNacimiento,$calle,$no,$colonia,$cp,$municipio,$gradoEstudios,$trabajo6Meses,$motivoNoTrabajo,$seguroMedico,$file){
+        public function __construct14($curp,$nombre,$telefono,$fechaNacimiento,$calle,$no,$colonia,$cp,$municipio,$gradoEstudios,$trabajo6Meses,$motivoNoTrabajo,$seguroMedico,$file){
             $this->curp = $curp;
             $this->nombre = $nombre;
             $this->telefono = $telefono;
@@ -36,7 +36,7 @@
             $this->file = $file;
         }
 
-        public function __construct16($idPadre,$curp,$nombre,$telefono,$fechaNacimiento,$calle,$no,$colonia,$cp,$municipio,$gradoEstudios,$trabajo6Meses,$motivoNoTrabajo,$seguroMedico,$file){
+        public function __construct15($idPadre,$curp,$nombre,$telefono,$fechaNacimiento,$calle,$no,$colonia,$cp,$municipio,$gradoEstudios,$trabajo6Meses,$motivoNoTrabajo,$seguroMedico,$file){
             $this->idPadre = $idPadre;
             $this->curp = $curp;
             $this->nombre = $nombre;
